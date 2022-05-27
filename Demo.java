6a521d62ebe16149ef8cf0e39f98dbd6d28b8286
Shubham Hiwale <shubhamhiwale4@gmail.com>
2022-05-27 17:23:06 +0530

    Demo.java created1

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@ class Demo
 {
 		public static void main(String argv[])
 				{
-							System.out.println("Hello");
+							System.out.println("Hello world");
 									System.out.println("Shubham Hiwale");
 				}}
